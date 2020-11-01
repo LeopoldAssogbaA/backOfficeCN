29b6a756745f35b95bb2cbbac4da739338ea410a
Léopold Assogba <leopoldassogba21@gmail.com>
2020-11-01 15:31:30 +0100

    init project

---
+++ craco.config.js
@@ -0,0 +1,21 @@
+const CracoLessPlugin = require("craco-less");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              "@primary-color": "#212529",
+              "@heading-color": "#212529",
+              "@text-color": "#212529",
+            },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.less";
+
+const App = () => {
+  return <div className="App">Init App</div>;
+};
+
+export default App;
